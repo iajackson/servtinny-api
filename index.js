0fa8502ef4679baac0e674d2d58f504e7399d59b
Ian Jackson <ian.jackson@redoxygen.com>
2024-06-28 14:33:44 +1000

    Update for ssl setup

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const express = require("express");
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var privateKey  = fs.readFileSync('sslcert/selfsigned.key');
+var certificate = fs.readFileSync('sslcert/selfsigned.crt');
 
 // Main Router
 const routes = require('./routes');
 
-const app = express();
+var credentials = {key: privateKey, cert: certificate};
+var express = require('express');
+var app = express();
 
 // Server middleware
 app.use((req, res, next) => {
@@ -14,6 +20,12 @@ app.use((req, res, next) => {
 // Main routes
 app.use('/', routes);
 
-app.listen(8080, () => {
- console.log("Server running on port 8080");
-});
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(8080, () => {
+  console.log("HTTP Server running on port 8080");
+ });
+httpsServer.listen(8443, () => {
+  console.log("HTTPS Server running on port 8443");
+ });
